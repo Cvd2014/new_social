8dd00fe67b4cbc5623a10f9f9d8869d8014f98c5
Ciaran Darcy <ciaran.darcy1@gmail.com>
2016-04-22 15:43:49 +0100

    added db

--- we_are_social/settings.py
+++ we_are_social/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -104,6 +105,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config("heroku_63a48651df32a90")
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
