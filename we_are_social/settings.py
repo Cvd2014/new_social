a6fa605053ca15a1ddfc8d7a7c6148d69d62c805
Ciaran Darcy <ciaran.darcy1@gmail.com>
2016-04-22 15:46:13 +0100

    db

--- we_are_social/settings.py
+++ we_are_social/settings.py
@@ -105,7 +105,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'] = dj_database_url.config("heroku_63a48651df32a90")
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
